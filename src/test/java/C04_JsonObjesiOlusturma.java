ba8746e4950813c7d0e0aad7bc2e539fb0ecff71
yusuf <yeed060118@gmail.com>
2023-12-29 01:51:54 +0300

    api

---
+++ src/test/java/C04_JsonObjesiOlusturma.java
@@ -0,0 +1,34 @@
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class C04_JsonObjesiOlusturma {
+    @Test
+    public void test01() {
+
+        /*
+        Asagidaki JSON Objesini olusturup konsolda
+        yazdirin.
+        {
+        "title":"Ahmet",
+        "body":"Merhaba",
+        "userId":1
+        }
+        */
+
+        JSONObject obj1 = new JSONObject();
+        obj1.put("title", "Yusuf");
+        obj1.put("body", "Merhaba");
+        obj1.put("userId", 1);
+
+        System.out.println("obj1 = " + obj1);
+/*
+        obj1 =
+        {
+        "title":"Yusuf",
+        "body":"Merhaba",
+        "userId":1
+        }
+ */
+
+    }
+}
