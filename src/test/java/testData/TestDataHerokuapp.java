575a86d5cd2fbcdbbe8e90631a5a203eea8096a3
yusuf <yeed060118@gmail.com>
2023-12-31 20:04:00 +0300

    api

--- src/test/java/testData/TestDataHerokuapp.java
+++ src/test/java/testData/TestDataHerokuapp.java
@@ -2,6 +2,9 @@ package testData;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestDataHerokuapp {
 
     public static JSONObject jsonRequestBodyOlustur() {
@@ -65,6 +68,71 @@ public class TestDataHerokuapp {
             }
         }
  */
+
+
+    public static Map<String, Object> requestBodyMapOlustur() {
+        Map<String, Object> requestBodyMap = new HashMap<>();
+        requestBodyMap.put("firstname", "Yusuf");
+        requestBodyMap.put("lastname", "Leon");
+        requestBodyMap.put("totalprice", 3500.0);
+        requestBodyMap.put("depositpaid", false);
+        requestBodyMap.put("bookingdates", bookingDatesMapOlustur());
+        requestBodyMap.put("additionalneeds", "wi-fi");
+
+        return requestBodyMap;
+    }
+
+
+    // inner map icin method olsuturuyorum --> yukaridaki methodda direkt kullanmak icin
+    public static Map<String, Object> bookingDatesMapOlustur() {
+        Map<String, Object> bookingdatesMap = new HashMap<>();
+        bookingdatesMap.put("checkin", "2021-06-01");
+        bookingdatesMap.put("checkout", "2021-06-10");
+
+        return bookingdatesMap;
+    }
+    /*
+    Request body
+        {
+        "firstname" : "Ahmet",
+        "lastname" : “Bulut",
+        "totalprice" : 500,
+        "depositpaid" : false,
+        "bookingdates" : {
+            "checkin" : "2021-06-01",
+            "checkout" : "2021-06-10"
+            },
+        "additionalneeds" : "wi-fi"
+        }
+     */
+
+
+    public static Map<String,Object> responseBodyMapOlustur(){
+        Map<String ,Object> responseBodyMap = new HashMap<>();
+        responseBodyMap.put("bookingid", 24);
+        responseBodyMap.put("booking", requestBodyMapOlustur());
+
+        return responseBodyMap;
+    }
+    /*
+    Response Body // expected data
+        {
+        "bookingid": 24,
+        "booking": {
+            "firstname": "Ahmet",
+            "lastname": "Bulut",
+            "totalprice": 500,
+            "depositpaid": false,
+            "bookingdates": {
+                "checkin": "2021-06-01",
+                "checkout": "2021-06-10"
+                },
+            "additionalneeds": "wi-fi"
+            }
+        }
+     */
+
+
 
 
 }
